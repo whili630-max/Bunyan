7afbfa91ae6dbf29b699ae81733305e160a1b754
Your Name <your-email@example.com>
2025-08-10 09:44:44 +0300

    chore: clean pubspec (remove assets/images), refresh deps

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter-sdk"
-export "FLUTTER_APPLICATION_PATH=C:\Users\0AGR\OneDrive\Desktop\flutter\New folder\Bunyan"
+export "FLUTTER_APPLICATION_PATH=C:\Projects\Bunyan"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
